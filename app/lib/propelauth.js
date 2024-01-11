8e891cc89e7ddbc3371c63ffe79770b030c8b099
PixelMothman <agustin.m.jessen@gmail.com>
2024-01-11 09:00:10 -0300

    v0.3.8.4 - Test

--- app/lib/propelauth.js
+++ app/lib/propelauth.js
@@ -1,7 +1,10 @@
-import { initBaseAuth } from "@propelauth/node";
+import { initBaseAuth} from "@propelauth/node";
 
 export const propelauth = initBaseAuth({
-  // You can find these fields under the Backend Integration section for your project in PropelAuth.
-  authUrl: process.env.NEXT_PUBLIC_AUTH_URL,
-  apiKey: process.env.PROPELAUTH_API_KEY
+    authUrl: process.env.NEXT_PUBLIC_AUTH_URL,
+    apiKey: process.env.PROPELAUTH_API_KEY,
+    manualTokenVerificationMetadata: {
+        verifierKey: process.env.PROPELAUTH_VERIFIER_KEY,
+        issuer: process.env.NEXT_PUBLIC_AUTH_URL,
+    }
 });
