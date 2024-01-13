0a2d0c05d051e27cdb31fb9b607a76df584e0ee9
PixelMothman <agustin.m.jessen@gmail.com>
2024-01-13 19:06:40 -0300

    v0.4.4 - New Login UI

--- app/page.js
+++ app/page.js
@@ -9,9 +9,16 @@ export default async function Home() {
       <div className="w-1/2 h-full">
         <Image priority src={loginImage} alt="login illustration" width={500} height={500} className='w-full h-full' />
       </div>
-      <div className="w-1/2 h-full flex flex-col items-center justify-center p-4 bg-neutral-800">
-        <div className='w-1/2 flex flex-col gap-4'>
-          <h1 className="text-2xl font-semibold text-zinc-200">Start now</h1>
+      <div className="w-1/2 h-full flex flex-col items-center justify-center p-4 bg-neutral-100 shadow-2xl shadow-black">
+        <div className='w-1/2 h-64 p-4 flex flex-col justify-between gap-4 border border-neutral-800 rounded-md'>
+          <div className='w-full h-fit flex flex-col gap-4'>
+            <h1 className="w-fit font-black text-4xl text-neutral-900">
+              NextJS Starter
+            </h1>
+            <p className="font-bold text-base text-neutral-900">
+              Welcome to NextJS Starter.
+            </p>
+          </div>
           <SignupAndLoginButtons />
         </div>
       </div>
--- app/ui/logInBtns.js
+++ app/ui/logInBtns.js
@@ -5,7 +5,7 @@ import {useRedirectFunctions} from "@propelauth/nextjs/client";
 export default function SignupAndLoginButtons() {
 	const {redirectToSignupPage, redirectToLoginPage} = useRedirectFunctions()
 	return <div className="flex flex-row gap-4">
-        <button onClick={() => redirectToLoginPage()} className="w-full h-12 bg-blue-400 hover:bg-blue-700 text-zinc-100 font-bold rounded-md">Log in</button>
-		<button onClick={() => redirectToSignupPage()} className="w-full h-12 bg-pink-300 hover:bg-pink-700 text-zinc-100 font-bold rounded-md">Sign up</button>
+        <button onClick={() => redirectToLoginPage()} className="w-full relative flex items-center justify-center px-4 py-2 text-base font-semibold text-white bg-neutral-800 border border-neutral-800 rounded-lg hover:ring-2 hover:ring-blue-500">Log in</button>
+		<button onClick={() => redirectToSignupPage()} className="w-full relative flex items-center justify-center px-4 py-2 text-base font-semibold text-black border border-neutral-800 rounded-lg hover:ring-2 hover:ring-blue-500">Sign up</button>
 	</div>
 }
