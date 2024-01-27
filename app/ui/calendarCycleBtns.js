644af8637b314d8ed08ed86914a6a3d743bac6ce
PixelMothman <agustin.m.jessen@gmail.com>
2024-01-27 10:50:19 -0300

    refactor: update calendar cycle button styles

--- app/ui/calendarCycleBtns.js
+++ app/ui/calendarCycleBtns.js
@@ -44,7 +44,7 @@ export default function CalendarCycleBtns({}){
             }
         }
         }
-        className="w-fit flex items-center justify-center px-2 py-2 border border-neutral-800 rounded-md shadow-sm text-neutral-800 font-bold hover:bg-neutral-800 hover:fill-neutral-100"
+        className="w-fit h-8 px-2 flex items-center justify-center border border-neutral-800 rounded-md shadow-sm text-neutral-800 font-bold hover:bg-neutral-800 hover:fill-neutral-100"
         >
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 256 256"><path d="M228,128a12,12,0,0,1-12,12H69l51.52,51.51a12,12,0,0,1-17,17l-72-72a12,12,0,0,1,0-17l72-72a12,12,0,0,1,17,17L69,116H216A12,12,0,0,1,228,128Z"></path></svg>
         </button>
@@ -65,7 +65,7 @@ export default function CalendarCycleBtns({}){
                 `)
             }
         }}
-        className="w-fit flex items-center justify-center px-2 py-2 border border-neutral-800 rounded-md shadow-sm text-neutral-800 font-bold hover:bg-neutral-800 hover:fill-neutral-100"
+        className="w-fit h-8 px-2 flex items-center justify-center border border-neutral-800 rounded-md shadow-sm text-neutral-800 font-bold hover:bg-neutral-800 hover:fill-neutral-100"
         >
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 256 256"><path d="M224.49,136.49l-72,72a12,12,0,0,1-17-17L187,140H40a12,12,0,0,1,0-24H187L135.51,64.48a12,12,0,0,1,17-17l72,72A12,12,0,0,1,224.49,136.49Z"></path></svg>
         </button>
