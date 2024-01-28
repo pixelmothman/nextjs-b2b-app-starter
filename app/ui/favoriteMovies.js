d7de410dff2faf4eb1820f882186d54de7658dc6
PixelMothman <agustin.m.jessen@gmail.com>
2024-01-28 09:29:22 -0300

    refactor: updated style of favorite movie divs in FavoriteMovies.

--- app/ui/favoriteMovies.js
+++ app/ui/favoriteMovies.js
@@ -11,13 +11,13 @@ export default async function FavoriteMovies(){
             <p className="text-neutral-800 mb-4">
             Simulating fetching from a database.
             </p>
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-row gap-4 flex-wrap">
             {
                 favMovies !== null && favMovies.length !== 0 ? (
                     favMovies.map((movie, index) => {
                         return (
-                            <div key={movie + index} className="w-full h-fit">
-                                <span className="text-base font-semibold text-neutral-800">
+                            <div key={movie + index} className="w-fit h-fit px-4 py-2 bg-neutral-100 rounded-md shadow-sm border border-neutral-800 cursor-default hover:outline hover:outline-neutral-300">
+                                <span className="text-sm font-semibold text-neutral-800">
                                     {movie}
                                 </span>
                             </div>
