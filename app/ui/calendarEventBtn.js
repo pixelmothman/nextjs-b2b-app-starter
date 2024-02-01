5d17bda746e2fa0654aea0d86a129323e6faf71a
PixelMothman <agustin.m.jessen@gmail.com>
2024-02-01 09:06:47 -0300

    refactor: Update CalendarEventBtn component styling and passed props.

--- app/ui/calendarEventBtn.js
+++ app/ui/calendarEventBtn.js
@@ -2,13 +2,13 @@
 
 import { useFormStatus } from 'react-dom'
 
-export default function CalendarEventBtn(){
+export default function CalendarEventBtn({btnText = 'Add event'}){
     const { pending } = useFormStatus();
 
     return (
-        <button type="submit" className="w-fit self-end px-4 py-2 bg-white border border-neutral-800 rounded-md shadow-sm text-sm font-bold text-neutral-800 hover:bg-neutral-800 hover:text-neutral-100 focus-visible:outline-0 focus-visible:ring-2 focus-visible:ring-black">
+        <button type="submit" className="w-fit self-end px-4 py-2 bg-white border border-neutral-800 rounded-md shadow-sm text-xs font-bold text-neutral-800 hover:bg-neutral-800 hover:text-neutral-100 focus-visible:outline-0 focus-visible:ring-2 focus-visible:ring-black">
         {
-            pending ? 'Loading...' : 'Add event'
+            pending ? 'Loading...' : btnText
         }
         </button>
     )
