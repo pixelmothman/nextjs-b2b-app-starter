47df348eea79aec6baceeb360902aa0028b38fb2
PixelMothman <agustin.m.jessen@gmail.com>
2024-02-06 14:03:48 -0300

    refactor: update image gallery container styling

--- app/dash/image-gallery/page.js
+++ app/dash/image-gallery/page.js
@@ -8,7 +8,7 @@ export default async function ImagesPage(){
         <div className="w-full h-full grid grid-cols-2 grid-rows-2 gap-4">
             <StyledDropzone/>
             <StyledDropzoneMultipleImages/>
-            <div className="col-span-2 max-h-[372px] p-5 rounded-sm bg-white border border-neutral-800 shadow-sm overflow-y-auto">
+            <div className="col-span-2 max-h-[372px] p-5 rounded-sm bg-white border border-neutral-800 shadow-sm overflow-hidden">
                 <ImageGallery/>
             </div>
         </div>
