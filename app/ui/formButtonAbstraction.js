fc525a4fd9aba3540b4fc19e81aef29189252377
PixelMothman <agustin.m.jessen@gmail.com>
2024-02-08 14:08:30 -0300

    fix: fix style in FormButtonAbstraction component

--- app/ui/formButtonAbstraction.js
+++ app/ui/formButtonAbstraction.js
@@ -2,7 +2,7 @@
 
 import { useFormStatus } from 'react-dom'
 
-export default function FormButtonAbstraction({loadingText, buttonText, buttonIcon, background = true}){
+export default function FormButtonAbstraction({loadingText, buttonText, buttonIcon, background = true }){
     const { pending } = useFormStatus();
 
     const buttonIcons = {
@@ -12,7 +12,7 @@ export default function FormButtonAbstraction({loadingText, buttonText, buttonIc
 
     return (
         <button type="submit" className={`${
-            background ? 'self-end w-fit px-4 py-2 border border-neutral-800 rounded-md shadow-sm text-sm font-bold text-neutral-800 hover:bg-neutral-800 hover:text-neutral-100  focus-visible:outline-0 focus-visible:ring-1 focus-visible:ring-black' : ''
+            background ? 'self-end w-fit px-4 py-2 border border-neutral-800 rounded-md shadow-sm text-sm font-bold text-neutral-800 hover:bg-neutral-800 hover:text-neutral-100 focus-visible:outline-0 focus-visible:ring-1 focus-visible:ring-black' : 'text-sm font-bold text-neutral-800 hover:underline hover:underline-offset-2'
         }`}>
         {
             pending ? loadingText : (
