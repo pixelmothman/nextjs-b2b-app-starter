c2c30fac24ba638d0c4b93a53656f779c648c678
PixelMothman <agustin.m.jessen@gmail.com>
2024-02-12 18:26:53 -0300

    refactor: update login page layout and content

--- app/page.js
+++ app/page.js
@@ -6,17 +6,17 @@ export default async function Home() {
     
   return (
     <div className="w-full h-full flex flex-row">
-      <div className="w-1/2 h-full">
+      <div className="hidden lg:flex w-1/2 h-full">
         <Image priority src={loginImage} alt="login illustration" width={500} height={500} className='w-full h-full' />
       </div>
-      <div className="w-1/2 h-full flex flex-col items-center justify-center p-4 bg-neutral-100 shadow-2xl shadow-black">
-        <div className='w-1/2 h-64 p-4 flex flex-col justify-between gap-4 border border-neutral-800 rounded-md'>
+      <div className="w-full lg:w-1/2 h-full flex flex-col items-center justify-center p-4 bg-neutral-100 shadow-2xl shadow-black">
+        <div className='w-[440px] h-64 p-4 flex flex-col justify-between gap-4 border border-neutral-800 rounded-md'>
           <div className='w-full h-fit flex flex-col gap-4'>
             <h1 className="w-fit font-black text-4xl text-neutral-900">
-              NextJS Starter
+              NextJS App Starter
             </h1>
             <p className="font-bold text-base text-neutral-900">
-              Welcome to NextJS Starter.
+              Created by <span href="https://twitter.com/pixelmothman" className='text-neutral-900 underline underline-offset-4 cursor-pointer'> @pixelmothman</span>
             </p>
           </div>
           <SignupAndLoginButtons />
