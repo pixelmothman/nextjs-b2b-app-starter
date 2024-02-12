9a2eafaad74a108714252bdedad9f767c700ac61
PixelMothman <agustin.m.jessen@gmail.com>
2024-02-12 18:25:22 -0300

    refactor: refactor login and signup buttons

--- app/ui/login/logInBtns.js
+++ app/ui/login/logInBtns.js
@@ -5,7 +5,11 @@ import {useRedirectFunctions} from "@propelauth/nextjs/client";
 export default function SignupAndLoginButtons() {
 	const {redirectToSignupPage, redirectToLoginPage} = useRedirectFunctions()
 	return <div className="flex flex-row gap-4">
-        <button onClick={() => redirectToLoginPage()} className="w-full relative flex items-center justify-center px-4 py-2 text-base font-semibold text-white bg-neutral-800 border border-neutral-800 rounded-lg hover:ring-2 hover:ring-blue-500">Log in</button>
-		<button onClick={() => redirectToSignupPage()} className="w-full relative flex items-center justify-center px-4 py-2 text-base font-semibold text-black border border-neutral-800 rounded-lg hover:ring-2 hover:ring-blue-500">Sign up</button>
+        <button onClick={() => redirectToLoginPage()} className='w-full px-4 py-2 border border-neutral-800 rounded-md shadow-sm text-sm font-bold text-neutral-800 hover:bg-neutral-800 hover:text-neutral-100 focus-visible:outline-0 focus-visible:ring-1 focus-visible:ring-black'>
+			Log in
+		</button>
+		<button onClick={() => redirectToSignupPage()} className="w-full px-4 py-2 border border-neutral-800 rounded-md shadow-sm text-sm font-bold text-neutral-800 hover:bg-neutral-800 hover:text-neutral-100 focus-visible:outline-0 focus-visible:ring-1 focus-visible:ring-black">
+			Sign up
+		</button>
 	</div>
 }
