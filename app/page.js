e9a3afd3a57ad7e09683d3e639ba64e67708147c
PixelMothman <agustin.m.jessen@gmail.com>
2024-02-17 19:36:06 -0300

    refactor: update login and sign up page layout and styling

--- app/page.js
+++ app/page.js
@@ -6,11 +6,9 @@ export default async function Home() {
     
   return (
     <div className="w-full h-full flex flex-row">
-      <div className="hidden lg:flex w-1/2 h-full">
-        <Image priority src={loginImage} alt="login illustration" width={500} height={500} className='w-full h-full' />
-      </div>
+      <div className="hidden lg:flex w-1/2 h-full bg-gradient-to-r from-neutral-200 to-neutral-900"/>
       <div className="w-full lg:w-1/2 h-full flex flex-col items-center justify-center p-4 bg-neutral-100 shadow-2xl shadow-black">
-        <div className='w-[440px] h-64 p-4 flex flex-col justify-between gap-4 border border-neutral-800 rounded-md'>
+        <div className='w-[440px] h-40 p-4 flex flex-col justify-between gap-4 border border-neutral-800 rounded-md'>
           <div className='w-full h-fit flex flex-col gap-4'>
             <h1 className="w-fit font-black text-xl text-neutral-900">
             pixelmothman/nextjs-b2b-app-starter
