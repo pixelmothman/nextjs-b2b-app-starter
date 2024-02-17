a5fd55f62596658776d5aef594178a782f996434
PixelMothman <agustin.m.jessen@gmail.com>
2024-02-17 19:27:36 -0300

    refactor: update login component UI.

--- app/page.js
+++ app/page.js
@@ -12,11 +12,11 @@ export default async function Home() {
       <div className="w-full lg:w-1/2 h-full flex flex-col items-center justify-center p-4 bg-neutral-100 shadow-2xl shadow-black">
         <div className='w-[440px] h-64 p-4 flex flex-col justify-between gap-4 border border-neutral-800 rounded-md'>
           <div className='w-full h-fit flex flex-col gap-4'>
-            <h1 className="w-fit font-black text-4xl text-neutral-900">
-              NextJS App Starter
+            <h1 className="w-fit font-black text-xl text-neutral-900">
+            pixelmothman/nextjs-b2b-app-starter
             </h1>
-            <p className="font-bold text-base text-neutral-900">
-              Created by <span href="https://twitter.com/pixelmothman" className='text-neutral-900 underline underline-offset-4 cursor-pointer'> @pixelmothman</span>
+            <p className="font-bold text-sm text-neutral-900">
+            🧪 created by <span href="https://twitter.com/pixelmothman" className='text-neutral-900 underline underline-offset-4 cursor-pointer'> @pixelmothman</span>
             </p>
           </div>
           <SignupAndLoginButtons />
