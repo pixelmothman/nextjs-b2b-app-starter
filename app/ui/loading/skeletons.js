a47c9ae4b33281263b18f937dd45c3d0e6fa2e0e
PixelMothman <agustin.m.jessen@gmail.com>
2024-02-17 19:10:04 -0300

    fix: correct shadow class for loading skeleton

--- app/ui/loading/skeletons.js
+++ app/ui/loading/skeletons.js
@@ -1,6 +1,6 @@
 export function GeneralLoading() {
     return (
-        <div className="w-full h-full flex items-center justify-center rounded-sm bg-white border border-neutral-800 shadow-s">
+        <div className="w-full h-full flex items-center justify-center rounded-sm bg-white border border-neutral-800 shadow-sm">
             <span className="text-base text-neutral-900 font-medium">
                 Loading...
             </span>
